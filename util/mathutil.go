698881fdf6adf5452b45904321c02fb82c7f8292
Vignesh <vignesh.saravanan@tekafforde.com>
2025-05-21 19:12:35 +0530

    fixed package name

--- util/mathutil.go
+++ util/mathutil.go
@@ -1,11 +1,11 @@
-package mathutil
+package util
 
 // Add adds two integers.
 func Add(a, b int) int {
-    return a + b
+	return a + b
 }
 
 // Multiply multiplies two integers.
 func Multiply(a, b int) int {
-    return a * b
+	return a * b
 }
